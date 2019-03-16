ba28aab452b3eb83e557768bb288d58491cecdbc
MattRubbelke <matt.rubbelke@gmail.com>
2019-03-16 14:09:39 -0500

    Updated java.

--- assets/Java/app.js
+++ assets/Java/app.js
@@ -1,7 +1,8 @@
 function seatGeek(){
     // $("#gifs-display").empty(); We made need this later to clear out the results available. 
-    var date =  $("#datepicker").val().trim();
-    var city =  $("#citypicker").val().trim();
+    var dateInput =  "03/23/2019"//$("#datepicker").val().trim();
+    var city =  "Minneapolis"//$("#citypicker").val().trim();
+    var date = moment(dateInput).format("YYYYMMDD") + "T00:00:00"
     console.log("This is the user's date input: "+ date)
     var queryURL = "https://api.seatgeek.com/2/events?datetime_utc=" + date + "&venue.city=" + city + "&client_id=MTU3MzU4MzB8MTU1MjQzODQ4My41Ng";
 
@@ -73,7 +74,7 @@ function getRestaurant(input, zip){
             console.log(restName);
             console.log(restCuisine);
             
-            var restDiv = $("<div class='rest-div' style='border-bottom: 2px gray solid;' id='"[j]"'>").append(
+            var restDiv = $("<div class='rest-div' style='border-bottom: 2px gray solid;' id='"+[j]+"'>").append(
                 $("<h5>").text(restName),
                 $("<p>").text("Cusine: " + restCuisine),
                 $("<p><a href="+restLink+">Click here check it out!</a></p>"),
